27aeb278744076eff90cd64cba56e8e04308d518
miles <mdnrosen@hotmail.co.uk>
2023-04-22 23:39:44 +0100

    had to use clickawaylistner to ... FUCKJ

--- src/components/QTitle.js
+++ src/components/QTitle.js
@@ -1,29 +1,39 @@
-import { Box, IconButton, Tooltip, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, ClickAwayListener, IconButton, Tooltip, Typography } from '@mui/material'
 import { Help } from '@mui/icons-material'
-import { React } from 'react'
 
 
 
 const QTitle = ({ title, num, help }) => {
+
+    const [ open, setOpen ] = useState(false)
+
+
     return (
         <Box component="span" sx={{ display: 'flex', alignItems: 'center'}}>
             <Typography variant="h6"><b>{num}. {title}</b></Typography>
             {help ?
-                <>
-                {/* This is desktop, tooltip on hover (default) */}
-                <Tooltip title={help} arrow placement="bottom-start" sx={{ display: { xs: 'none', md: 'flex' }}}>
-                    <IconButton>
-                        <Help /> 
-                    </IconButton>
-                </Tooltip>
-
-                {/* This is the mobile one with tooltip on tap */}
-                <Tooltip title={help} arrow placement="bottom-start" sx={{ display: { xs: 'flex', md: 'none' }}}>
-                    <IconButton>
-                        <Help /> 
-                    </IconButton>
-                </Tooltip>
-                </>
+                <ClickAwayListener onClickAway={() => setOpen(false)} sx={{ display: { xs: 'flex', md: 'none' }}}>
+                    <div>
+                        <Tooltip
+                            PopperProps={{
+                                disablePortal: true,
+                            }}
+                            onClose={() => setOpen(false)}
+                            open={open}
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            title={help}    
+                            arrow 
+                            placement="bottom-start" 
+                        >
+                            <IconButton onClick={() => setOpen(true)}>
+                                <Help /> 
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                </ClickAwayListener>
                 :
                 null
             }
